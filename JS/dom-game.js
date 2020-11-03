a5e1e4d978340ca357b43da7591f7235a1d0c1ae
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-11-03 13:02:11 -0600

    trying to create a basic idea of the logic i was thinking for the game

--- JS/dom-game.js
+++ JS/dom-game.js
@@ -1,3 +1,55 @@
-(function (){
+// (function (){
+// })();
 
-})();
+// game start button
+const startGame = document.getElementById("startGame");
+const guessBox = document.getElementById("guessBox");
+const assertCode = document.getElementById("assert");
+
+// elements to manipulate
+let headingSection = document.getElementById("nameToAdd");
+
+let listSection = document.getElementById("listToEdit");
+
+startGame.addEventListener("click", function(){
+
+   let randomNum = Math.floor(Math.random() * 2) + 1;
+
+
+   if(randomNum === 1){
+       headingSection.style.display = "block";
+       listSection.style.display = "none";
+   } else if(randomNum === 2){
+       listSection.style.display = "block";
+       headingSection.style.display = "none";
+   }
+
+   assertCode.addEventListener("click", function (){
+      //something about guessBox.value applying to the innerHTML of
+      // whichever element there???
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
+
+
+
+
+
+
+
+

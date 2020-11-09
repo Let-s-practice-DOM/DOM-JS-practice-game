01dc0741c5b2cf67eb5584840a5966a83feb1ada
Matthew Baker <Matthew.d.baker20@gmail.com>
2020-11-09 16:15:41 -0600

    moved canvas to center of screen

--- JS/dom-game.js
+++ JS/dom-game.js
@@ -22,27 +22,23 @@
             }
         }
     }
-    setInterval(load, 50)
+    setInterval(load, 50);
     function load (){
         draw()
     }
     function draw(){
-        fill(0,0,600,800, "black") //This is for canvas
-        fill(pacman.x, pacman.y, 25, 25, "yellow")//this is for the pacman
+        fill(0,0, canvas.width, canvas.height, "black"); //This is for canvas
+        fill(pacman.x, pacman.y, 25, 25, "yellow");//this is for the pacman
     }
 
     function fill(x, y, w, h, c){
         context.fillStyle = c;
-        context.fillRect(x,y, w, h);
+        context.fillRect(x, y, w, h);
 
     }
     window.addEventListener("keydown", function (e){
        const direction = e.key.replace("Arrow", "");
-       pacMan.move(direction);
-
+       pacman.move(direction);
     });
-
-
-
 
 })();
